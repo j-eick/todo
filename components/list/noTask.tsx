bde760003ee0d5305fa8cf801e95fdad7825190c
Joschka Eickhoff <jeickhoff@posteo.de>
2023-10-30 14:25:00 +0100

    minor changes

--- components/list/noTask.tsx
+++ components/list/noTask.tsx
@@ -1,17 +1,19 @@
 export default function NoTasks() {
   return (
-    <div
-      className="relative p-5 rounded-lg bg-slate-100 shadow-md
-    xs:w-[80%] xs:h-[50%]
+    <div className="relative h-[100dvh]">
+      <div
+        className="absolute left-1/2 -translate-x-1/2 top-[10%]  p-5 rounded-lg shadow-md bg-white
+    xs:w-[80%] xs:h-[30%]
     "
-    >
-      <p>
-        Congrats, <br />
-        you've got nothing to do... <br />
-      </p>
-      <span className="absolute left-1/2 top-1/2 -translate-x-1/2 text-lg">
-        ...yet!
-      </span>
+      >
+        <p>
+          Congrats, <br />
+          you've got nothing to do... <br />
+        </p>
+        <span className="absolute left-1/2 top-1/2 -translate-x-1/2 text-lg">
+          ...yet!
+        </span>
+      </div>
     </div>
   );
 }
