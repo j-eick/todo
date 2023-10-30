544f07926e59fde31736a7c944a9a89508e6dcec
Joschka Eickhoff <jeickhoff@posteo.de>
2023-10-30 19:02:14 +0100

    color fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TaskList from "@/components/list/taskList";
 
 export default function Home() {
   return (
-    <main className="items-center">
+    <main className="relative flex justify-center">
       <TaskList />
       <Input_AddTask />
     </main>
--- components/list/tasks.tsx
+++ components/list/tasks.tsx
@@ -6,16 +6,18 @@ export default function Tasks() {
   return (
     <>
       <ul
-        className="sticky overflow-y-auto max-h-[57dvh] mt-10 py-10
-    flex items-center flex-col gap-3 
-    sm:max-h-[70dvh] sm:mt-20
+        className="sticky overflow-y-auto max-h-[57dvh] mt-10 py-5 rounded-xl
+        flex items-center flex-col gap-3 
+        bg-teal-50 bg-opacity-50
+        sm:max-h-[70dvh] sm:mt-20
     "
       >
         {allTodos.map((todo, index) => (
           <li
             key={index}
-            className="w-[70%] p-3 px-5 rounded-xl bg-white 
-            bg-opacity-100 backdrop-blur-[2rem]"
+            className="w-[80%] p-3 px-5 rounded-xl bg-slate-50 
+            bg-opacity-100 backdrop-blur-[2rem] task-shadow
+            "
           >
             <div>{todo.todo}</div>
           </li>
