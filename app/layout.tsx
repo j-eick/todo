bf5881eaf6754a4cbac1da6359c05d8c3e3ef440
Joschka Eickhoff <jeickhoff@posteo.de>
2023-10-31 01:02:55 +0100

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} relative w-full h-[60dvh];
+        className={`${inter.className} relative w-full
+        xs:h-[533px]
       `}
       >
         <div
